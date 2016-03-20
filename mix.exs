65021df0502fb58cf9331e02166968266aed64af
David Gu <david.gurquiza@gmail.com>
2016-03-20 16:22:57 -0700

    Elixir 1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Davo.Mixfile do
   def project do
     [app: :davo,
      version: "0.0.1",
-     elixir: "~> 1.0",
+     elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
