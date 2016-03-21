89016f05d42fb5d7961cb34eb119fcbe30e2cd1f
David Gu <david.gurquiza@gmail.com>
2016-03-21 00:15:04 -0700

    Attempt to allow sockets from subdomains
    Warning: This may be dangerous
    https://groups.google.com/forum/#!msg/phoenix-talk/5P0zkdrMZOU/P2lL9DpLAAAJ

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule Davo.UserSocket do
   channel "users:*", Davo.UsersChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    check_origin: false
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
